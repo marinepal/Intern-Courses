ea739017ba849b6f608e7c2e80cfc078a2b10058
Marine <marine@intelinair.com>
2021-06-22 13:51:08 +0400

    Reformatted code

--- edx Intro CS and Python/Problem 1.3.py
+++ edx Intro CS and Python/Problem 1.3.py
@@ -1,16 +1,16 @@
 # Assuming s is a string of lower case characters,
 # the program prints the longest substring of s in which the letters occur in alphabetical order.
 # s will be defined below to avoid unresolved reference errors
-s = "azcbobobegghakl" # the answer to this test case is beggh
+s = "azcbobobegghakl"  # the answer to this test case is beggh
 start = 0
 length = 1
 index = 0
 while index < len(s) - 1:
-    if s[index] <= s[index+1]:
+    if s[index] <= s[index + 1]:
         temp_start = index
         temp_length = 2
         index += 1
-        while index + 1 < len(s) and s[index] <= s[index+1]:
+        while index + 1 < len(s) and s[index] <= s[index + 1]:
             temp_length += 1
             index += 1
         if temp_length > length:
